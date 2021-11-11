083ee642137c8f8609ec8b0c943005e166551c1a
Alma-Alfaro <94043214+Alma-Alfaro@users.noreply.github.com>
2021-11-10 22:10:07 -0600

    trabajos terminados

---
+++ UNIDAD1/DISEÑORESPONSIVO/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Roux Academy</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+<body>
+<div class="contenido">
+<header>
+	<img src="images/ralogo_monogram.png">
+	<p id="descrip" >Roux Academy <br> Art-Media-Desing</p>
+	<p  > &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Lorem ipsum dolor sit amet, consectetur adipisicing elit, &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 
+		&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 
+		&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+		&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;quis nostrud exercitation ullamco laboris nisi</p>
+</header>
+<nav>
+	<ul>
+		<a href="#indice">HOME</a>
+		<a href="subdirectorio/artist.html">ARTIST</a> 
+		<a href="subdirectorio/schedule.html">SCHEDULE</a> 
+		<a href="subdirectorio/venue-travel.html">VENUE/TRAVEL</a> 
+		<a href="subdirectorio/register.html">REGISTER</a> 
+	</ul>
+</nav>
+<img width="790" height="200" src="images/art01.jpg">
+<div class="principal">
+			<main>
+				<h2>About the Event</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<h2>Feactured Artists</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <div class="cuadricula"> 
+				<div class="fila">
+                      <div class="columna">
+                      <img class="respon" src="images/artists/Barot_Bellingham_tn.jpg">
+                      </div>
+                      <div class="columna">
+                      <img class="respon" src="images/artists/Jennifer_Jerome_tn.jpg">
+                      </div>
+                      <div class="columna">
+                      <img class="respon" src="images/artists/Constance_Smith_tn.jpg">
+                      </div>
+                      
+                </div>
+                <div class="fila">
+                      <div class="columna">
+                      <img class="respon" src="images/artists/Jonathan_Ferrar_tn.jpg">
+                      </div>
+                      <div class="columna">
+                      <img class="respon" src="images/artists/Riley_Rewington_tn.jpg">
+                      </div>
+                      <div class="columna">
+                      <img class="respon" src="images/artists/LaVonne_LaRue_tn.jpg">
+                      </div>
+                      
+                </div>
+                <div class="fila">
+                     <div class="columna">
+                     <img  class="respon" src="images/artists/Hassum_Harrod_tn.jpg">
+                     </div>
+                     <div class="columna">
+                     <img class="respon" src="images/artists/Hillary_Goldwynn_tn.jpg">
+                     </div>
+                     <div class="columna">
+                      <img class="respon" src="images/artists/Xhou_Ta_tn.jpg">
+                      </div>
+                     
+                </div>
+                </div>	
+                <br>
+                <a href="subdirectorio/artist.html" class="beside" style="float: right;">View Artists Info >> </a>
+                <br>
+
+                <h2> The Venue </h2>
+				<div class="secciones">
+				<section>
+					<h3>Hotel Contempo</h3>	
+					<img src="images/hotel_contempo.jpg">
+				</section>
+
+				<section>
+					<br><br><br>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</section>
+
+				<section>
+                				
+				</section>
+
+				</div>
+			</main>
+
+			<aside>
+				<article>
+					<h2 id="red"> Coming to the event? </h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				</article>
+				<article>
+					<img src="images/iphone.png" width="225">
+					<a href="#" class="beside" style="float: right;">Roux Mobile >> </a>
+				</article>
+				<article>
+					<h2> Schedule </h2>
+					<h3>Monday</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor</p>
+					<h3>Thuesday</h3>
+					<p> in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				</article>
+				<a href="#" class="beside" style="float: right;">Full Schedule >> </a>
+			</aside>	
+		</div>
+    <header>
+	<img src="images/ralogo_monogram.png">
+	<p id="descrip" >Roux Academy <br> Art-Media-Desing</p>
+    </header>
+
+</div>
+
+</body>
+</html>
